1ec94abbe3bed701ee8ec944658102a076ccf0c1
shabani2 <shabanibernard1@gmail.com>
2024-10-27 20:35:50 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="container flex items-start justify-start">
+  <div className='bg-gray-500'>
+    welcome to a starter projectx
+  </div>
+  <div>
+    <Button className="bg-blue-800 text-gray-200">shadcn btn</Button>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+	content: [
+	  './src/**/*.{js,jsx,ts,tsx}',
+	],
+	theme: {
+	  extend: {		
+			colors: {
+			  border: '#d1d5db',      // Couleur personnalisée pour les bordures
+			  background: '#f3f4f6',  // Couleur de fond personnalisée
+			  foreground: '#111827',  // Couleur du texte personnalisée
+		
+		  },
+	  },
+	},
+	plugins: [],
+  }
+  
