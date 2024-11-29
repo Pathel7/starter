4799d5a3ab8175ca90f2a9129c09476a442e7354
shabani2 <shabanibernard1@gmail.com>
2024-11-29 12:06:38 +0100

    mise en place de store

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './Slices/users/userSlice'
+import eleveReducer from './Slices/users/eleveSlice'
+import parentReducer from './Slices/users/parentSlice'
+import professeurReducer from './Slices/users/professeurSlice'
+import ecoleReducer from './Slices/ecoles/ecoleSlice'
+import coursReducer from './Slices/ecoles/coursSlice'
+import classeReducer from './Slices/ecoles/classeSlice'
 
 export const Store = configureStore({
   reducer: {
-   
+   users: userReducer,
+   eleves : eleveReducer,
+   parents: parentReducer,
+   professeurs : professeurReducer,
+   ecoles : ecoleReducer,
+   classes : classeReducer,
+   cours : coursReducer
    
   
   },
